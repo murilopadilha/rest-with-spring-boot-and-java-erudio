24ac6dc565a550c9963894542a2d08074762bd6c
Murilo Miorim <murilo.p.miorim@gmail.com>
2026-08-01 15:52:10 -0300

    working with logs

--- src/main/java/br/com/erudio/rest_with_spring_boot_and_java_erudio/services/PersonServices.java
+++ src/main/java/br/com/erudio/rest_with_spring_boot_and_java_erudio/services/PersonServices.java
@@ -3,19 +3,20 @@ package br.com.erudio.rest_with_spring_boot_and_java_erudio.services;
 import br.com.erudio.rest_with_spring_boot_and_java_erudio.exception.ResourceNotFoundException;
 import br.com.erudio.rest_with_spring_boot_and_java_erudio.model.Person;
 import br.com.erudio.rest_with_spring_boot_and_java_erudio.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository repository;
